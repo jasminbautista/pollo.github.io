4e0ba795e782b575ebd0fb04b7eafa1529dc189e
jasminbautista <139304253+jasminbautista@users.noreply.github.com>
2023-08-21 09:26:04 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,469 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i"
+		rel="stylesheet">
+
+	<!-- Titulo de el sitio-->
+	<title>Campero</title>
+
+	<link rel="shortcut icon" type="image/icon" href="" />
+
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="assets/css/linearicons.css">
+
+	<link rel="stylesheet" href="assets/css/animate.css">
+
+	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+
+	<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+	<link rel="stylesheet" href="assets/css/bootsnav.css">
+
+	<link rel="stylesheet" href="assets/css/style.css">
+
+	<link rel="stylesheet" href="assets/css/responsive.css">
+
+</head>
+
+<body>
+
+	<header id="home" class="welcome-hero">
+
+		<div id="header-carousel" class="carousel slide carousel-fade" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#header-carousel" data-slide-to="0" class="active"><span class="small-circle"></span>
+				</li>
+				<li data-target="#header-carousel" data-slide-to="1"><span class="small-circle"></span></li>
+				<li data-target="#header-carousel" data-slide-to="2"><span class="small-circle"></span></li>
+			</ol>
+			<div class="carousel-inner" role="listbox">
+				<div class="item active">
+					<div class="single-slide-item slide1">
+						<div class="container">
+							<div class="welcome-hero-content">
+								<div class="row">
+									<div class="col-sm-7">
+										<div class="single-welcome-hero">
+											<div class="welcome-hero-txt">
+												<h4>PROMOCIÓN</h4>
+												<h2>SOLO HOY</h2>
+												<p>
+													Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+													eiuiana smod tempor ut labore et dolore magna aliqua. Ut enim ad
+													minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+													aliquip.
+												</p>
+												<div class="packages-price">
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="col-sm-5">
+										<div class="single-welcome-hero">
+											<div class="welcome-hero-img">
+												<img src="assets/images/slider/pollo2.jpg" alt="slider image">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+				<div class="item">
+					<div class="single-slide-item slide2">
+						<div class="container">
+							<div class="welcome-hero-content">
+								<div class="row">
+									<div class="col-sm-7">
+										<div class="single-welcome-hero">
+											<div class="welcome-hero-txt">
+												<h4>PROMOCIÓN</h4>
+												<h2>SOLO HOY</h2>
+												<p>
+													Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+													eiuiana smod tempor ut labore et dolore magna aliqua. Ut enim ad
+													minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+													aliquip.
+												</p>
+												<div class="packages-price">
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="col-sm-5">
+										<div class="single-welcome-hero">
+											<div class="welcome-hero-img">
+												<img src="assets/images/slider/pollo1.jpg" alt="slider image">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+				<div class="item">
+					<div class="single-slide-item slide3">
+						<div class="container">
+							<div class="welcome-hero-content">
+								<div class="row">
+									<div class="col-sm-7">
+										<div class="single-welcome-hero">
+											<div class="welcome-hero-txt">
+												<h4>PROMOCIÓN</h4>
+												<h2>SOLO HOY</h2>
+												<p>
+													Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+													eiuiana smod tempor ut labore et dolore magna aliqua. Ut enim ad
+													minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+													aliquip.
+												</p>
+												<div class="packages-price">
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="col-sm-5">
+										<div class="single-welcome-hero">
+											<div class="welcome-hero-img">
+												<img src="assets/images/slider/pollo3.jpg" alt="slider image">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<div class="top-area">
+			<div class="header-area">
+				<!-- Inicio navegación-->
+				<nav class="navbar navbar-default bootsnav  navbar-sticky navbar-scrollspy"
+					data-minus-value-desktop="70" data-minus-value-mobile="55" data-speed="1000">
+
+					<!-- Start Top Search -->
+					<div class="top-search">
+						<div class="container">
+							<div class="input-group">
+								<span class="input-group-addon"><i class="fa fa-search"></i></span>
+								<input type="text" class="form-control" placeholder="Search">
+								<span class="input-group-addon close-search"><i class="fa fa-times"></i></span>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="attr-nav">
+							<ul>
+								<li class="search">
+									<a href="#"><span class="lnr lnr-magnifier"></span></a>
+								</li><!--/.search-->
+							</ul>
+						</div>
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse"
+								data-target="#navbar-menu">
+								<i class="fa fa-bars"></i>
+							</button>
+							<a class="navbar-brand" href="index.html">CAM<span>PERO</a>
+
+						</div><!--/.navbar-header-->
+						<!-- End Header Navigation -->
+						<div class="collapse navbar-collapse menu-ui-design" id="navbar-menu">
+							<ul class="nav navbar-nav navbar-center" data-in="fadeInDown" data-out="fadeOutUp">
+								<li class=" scroll active"><a href="#home">Promociones</a></li>
+								<li class="scroll"><a href="#new-arrivals">Menú</a></li>
+								<li class="scroll"><a href="#feature">Ubicación</a></li>
+								<li class="scroll"><a href="#blog">Nosotros</a></li>
+								<li class="scroll"><a href="#newsletter">Redes Sociales</a></li>
+							</ul><!--/.nav -->
+						</div><!-- /.navbar-collapse -->
+					</div><!--/.container-->
+				</nav><!--/nav-->
+				<!-- Final Navigatición -->
+			</div><!--/.header-area-->
+			<div class="clearfix"></div>
+		</div><!-- /.top-area-->
+		<!-- top-area End -->
+
+	</header><!--/.welcome-hero-->
+	<!--welcome-hero end -->
+
+	<!--new-arrivals start -->
+	<section id="new-arrivals" class="new-arrivals">
+		<div class="container">
+			<div class="section-header">
+				<h2>Menú</h2>
+			</div><!--/.section-header-->
+			<div class="new-arrivals-content">
+				<div class="row">
+					<div class="col-md-3 col-sm-4">
+						<div class="single-new-arrival">
+							<div class="single-new-arrival-bg">
+								<img src="assets/images/menu/pollo1.jpg" alt="new-arrivals images">
+								<div class="single-new-arrival-bg-overlay"></div>
+							</div>
+							<?php
+                             $conex = new PDO('mysql:host=localhost; dbname=campero', 'root', '');
+                             $statement = $conex->prepare('SELECT * FROM plato WHERE idplato = 1');
+                             $statement->execute(); 
+   
+                             while ($fila = $statement->fetch(PDO::FETCH_ASSOC)) {
+                                 echo '<h4><a href="#">' . $fila['nombre_plato'] . '</a></h4>';
+                                 echo '<p class="arrival-product-price">' . $fila['descripcion'] . '</p>';
+                             }
+                            ?>
+
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="single-new-arrival">
+							<div class="single-new-arrival-bg">
+								<img src="assets/images/menu/pollo3.jpg" alt="new-arrivals images">
+								<div class="single-new-arrival-bg-overlay"></div>
+							</div>
+							<?php
+                             $conex = new PDO('mysql:host=localhost; dbname=campero', 'root', '');
+                             $statement = $conex->prepare('SELECT * FROM plato WHERE idplato = 2');
+                             $statement->execute(); 
+   
+                             while ($fila = $statement->fetch(PDO::FETCH_ASSOC)) {
+                                 echo '<h4><a href="#">' . $fila['nombre_plato'] . '</a></h4>';
+                                 echo '<p class="arrival-product-price">' . $fila['descripcion'] . '</p>';
+                             }
+                          ?>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="single-new-arrival">
+							<div class="single-new-arrival-bg">
+								<img src="assets/images/menu/pollo2.jpg" alt="new-arrivals images">
+								<div class="single-new-arrival-bg-overlay"></div>
+							</div>
+							<?php
+                             $conex = new PDO('mysql:host=localhost; dbname=campero', 'root', '');
+                             $statement = $conex->prepare('SELECT * FROM plato WHERE idplato = 3');
+                             $statement->execute(); 
+   
+                             while ($fila = $statement->fetch(PDO::FETCH_ASSOC)) {
+                                 echo '<h4><a href="#">' . $fila['nombre_plato'] . '</a></h4>';
+                                 echo '<p class="arrival-product-price">' . $fila['descripcion'] . '</p>';
+                             }
+                            ?>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="single-new-arrival">
+							<div class="single-new-arrival-bg">
+								<img src="assets/images/menu/pollo4.jpg" alt="new-arrivals images">
+								<div class="single-new-arrival-bg-overlay"></div>
+							</div>
+							<h4><a href="#">Nombre</a></h4>
+							<p class="arrival-product-price">$00.00</p>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="single-new-arrival">
+							<div class="single-new-arrival-bg">
+								<img src="assets/images/menu/pasta.png" alt="new-arrivals images">
+								<div class="single-new-arrival-bg-overlay"></div>
+							</div>
+							<h4><a href="#">Pasta</a></h4>
+							<p class="arrival-product-price">$00.00</p>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="single-new-arrival">
+							<div class="single-new-arrival-bg">
+								<img src="assets/images/menu/pure.jpg" alt="new-arrivals images">
+								<div class="single-new-arrival-bg-overlay"></div>
+							</div>
+							<h4><a href="#">Pure papa</a></h4>
+							<p class="arrival-product-price">$00.00</p>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="single-new-arrival">
+							<div class="single-new-arrival-bg">
+								<img src="assets/images/menu/totopos.jpg" alt="new-arrivals images">
+								<div class="single-new-arrival-bg-overlay"></div>
+							</div>
+							<h4><a href="#">Totopos</a></h4>
+							<p class="arrival-product-price">$00.00</p>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="single-new-arrival">
+							<div class="single-new-arrival-bg">
+								<img src="assets/images/menu/ensalada.jpg" alt="new-arrivals images">
+								<div class="single-new-arrival-bg-overlay"></div>
+							</div>
+							<h4><a href="#">Ensalada</a></h4>
+							<p class="arrival-product-price">$00.00</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/.container-->
+
+	</section>
+	<section>
+		<div class="container">
+			<div class="section-header">
+				<h2>Acompañamientos</h2>
+			</div><!--/.section-header-->
+			<div class="feature-content">
+				<div class="row">
+					<div class="col-sm-3">
+						<div class="single-feature">
+							<img src="assets/images/acompañamientos/totopos.jpg" alt="feature image">
+							<div class="single-feature-txt text-center">
+								<h3><a href="">Totopos</a></h3>
+								<h5>$00.00</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="single-feature">
+							<img src="assets/images/acompañamientos/pasta.jpg" alt="feature image">
+							<div class="single-feature-txt text-center">
+								<h3><a href="#">Pasta</a></h3>
+								<h5>$00.00</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="single-feature">
+							<img src="assets/images/acompañamientos/pure.jpg" alt="feature image">
+							<div class="single-feature-txt text-center">
+								<h3><a href="#">Pure</a></h3>
+								<h5>$00.00</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="single-feature">
+							<img src="assets/images/acompañamientos/salsas.jpg" alt="feature image">
+							<div class="single-feature-txt text-center">
+								<h3><a href="#">Salsas</a></h3>
+								<h5>$00.00</h5>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/.container-->
+		</div>
+	</section>
+
+	<!--feature start -->
+	<section id="feature" class="feature">
+		<div class="container">
+			<div class="section-header">
+				<h2>Ubicación</h2>
+			</div><!--/.section-header-->
+			<div class="feature-content">
+				<iframe
+					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d377.3208125635893!2d-99.17849944199597!3d17.59790561789666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85c95862ad921aad%3A0xff8254ea5064a926!2sC.%2011%20Nte.%2017%2C%20Jardines%2C%2041104%20Chilapa%20de%20%C3%81lvarez%2C%20Gro.!5e0!3m2!1ses-419!2smx!4v1689118616352!5m2!1ses-419!2smx"
+					width="1100" height="300" style="border:0;" allowfullscreen="" loading="lazy"
+					referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</div>
+		</div><!--/.container-->
+
+	</section>
+	<section id="blog" class="blog">
+		<div class="container">
+			<div class="section-header">
+				<h2>Noticias</h2>
+			</div><!--/.section-header-->
+			<div class="blog-content">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="single-blog">
+							<div class="single-blog-txt">
+								<h2><a href="#">Horarios de atención</a></h2>
+								<p>
+									D
+								</p>
+							</div>
+							<div class="single-blog-img">
+								<img src="assets/images/nosotros/horario.png" alt="blog image">
+								<div class="single-blog-img-overlay"></div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="single-blog">
+							<div class="single-blog-txt">
+								<h2><a href="#">Servicio a Domicilio</a></h2>
+								<p>
+									ll
+								</p>
+								<div class="single-blog-img">
+									<img src="assets/images/nosotros/domicilio.png" alt="blog image">
+									<div class="single-blog-img-overlay"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</section>
+	<section id="newsletter" class="newsletter">
+		<div class="container">
+			<footer id="footer" class="footer">
+				<div class="container">
+					<div class="hm-footer-copyright text-center">
+						<div class="footer-social">
+							<a href="#"><i class="fa fa-whatsapp"></i></a>
+							<a href="https://www.facebook.com/profile.php?id=100089126659533"><i
+									class="fa fa-facebook"></i></a>
+							<a href="#"><i class="fa fa-instagram"></i></a>
+						</div>
+						<p>
+							&copy; <a href="https://www.themesine.com/"></a>
+						</p>
+					</div>
+				</div>
+
+				<div id="scroll-Top">
+					<div class="return-to-top">
+						<i class="fa fa-angle-up " id="scroll-top" data-toggle="tooltip" data-placement="top" title=""
+							data-original-title="Back to Top" aria-hidden="true"></i>
+					</div>
+
+				</div>
+			</footer>
+		</div>
+
+	</section>
+
+
+	<script src="assets/js/jquery.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/bootsnav.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+	<script src="assets/js/custom.js"></script>
+
+</body>
+
+</html>
